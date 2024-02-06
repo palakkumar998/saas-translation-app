8dbc7da83ea126f9e0c9f9f62bbe475725cedc20
Palak kumar <116839013+palakkumar998@users.noreply.github.com>
2024-02-06 21:51:26 +0530

    Logo and Header setup

---
+++ components/Header.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Logo from './Logo'
+
+function Header() {
+    return (
+        <header>
+            <nav>
+                <Logo />
+            </nav>
+            Header
+        </header>
+    )
+}
+
+export default Header
---
+++ components/Logo.tsx
@@ -0,0 +1,10 @@
+import LogoImage from "@logos/icons8-logo.svg";
+import Link from "next/link";
+
+function Logo() {
+    return (
+        <Link href="/">Go Home    </Link>
+    )
+}
+
+export default Logo
