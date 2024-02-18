5c67d1a11bfb74f3575fe6206965b3fcf52f341e
Palak kumar <116839013+palakkumar998@users.noreply.github.com>
2024-02-19 00:00:48 +0530

    code enhance ment & landing page

--- components/Header.tsx
+++ components/Header.tsx
@@ -17,7 +17,7 @@ async function Header() {
 
 
     return (
-        <header className='sticky top-0 z-50' >
+        <header className='sticky top-0 z-50 ' >
             <nav className='flex flex-col sm:flex-row items-center justify-center p-5 pl-2  bg-white dark:bg-gray-900 max-w-7xl mx-auto ' >
                 <Logo />
                 <div className='flex-1  flex items-center justify-end space-x-2 ' >
@@ -53,7 +53,6 @@ async function Header() {
 
             {/* Upgrade section  */}
 
-            Header
         </header>
     )
 }
