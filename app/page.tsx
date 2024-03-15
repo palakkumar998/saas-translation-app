28fa03a3ef796f4476cfdbd33398073225533738
Palak kumar <116839013+palakkumar998@users.noreply.github.com>
2024-03-16 00:18:45 +0530

    implementing firebase admin (id, and firebase token fetched)

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,17 @@
+import { authOptions } from "@/auth";
+import { getServerSession } from "next-auth";
 
-export default function Home() {
+export default async function Home() {
+
+  const session = await getServerSession(authOptions);
+  console.log(session)
+
 
   return (
     <main className="">
       <div className="relative overflow-hidden before:absolute before:top-0 before:start-1/2 before:bg-[url('https://preline.co/assets/svg/examples/squared-bg-element.svg')] before:bg-no-repeat before:bg-top before:size-full before:-z-[1] before:transform before:-translate-x-1/2 dark:before:bg-[url('https://preline.co/assets/svg/examples-dark/squared-bg-element.svg')]">
         <div className="max-w-[85rem] mx-auto px-4 sm:px-6 lg:px-8 pt-24 pb-10">
-       
+
           <div className="flex justify-center">
             <a className="inline-flex items-center gap-x-2 bg-white border border-gray-200 text-xs text-gray-600 p-2 px-3 rounded-full transition hover:border-gray-300 dark:bg-gray-800 dark:border-gray-700 dark:hover:border-gray-600 dark:text-gray-400" href="#">
               Explore our featured subscription plans
--- auth.ts
+++ auth.ts
@@ -1,5 +1,7 @@
 import { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import { adminAuth, adminDb } from "./firebase-admin";
+import { FirestoreAdapter } from "@auth/firebase-adapter";
 
 export const authOptions: NextAuthOptions = {
     providers: [
@@ -11,9 +13,33 @@ export const authOptions: NextAuthOptions = {
         }),
 
     ],
+
+    callbacks: {
+        session: async ({ session, token }) => {
+            if (session?.user) {
+                if (token.sub) {
+                    session.user.id = token.sub;
+
+                    const firebaseToken = await adminAuth.createCustomToken(token.sub);
+                    session.firebaseToken = firebaseToken;
+                }
+            }
+            return session;
+        },
+        jwt: async ({ user, token }) => {
+            if (user) {
+                token.sub = user.id;
+            }
+            return token;
+        },
+    },
+
     session: {
         strategy: 'jwt'
-    }
+    },
+
+    adapter: FirestoreAdapter(adminDb),
+
 
 
 } satisfies NextAuthOptions
